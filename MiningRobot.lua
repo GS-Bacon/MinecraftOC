2f15c204e3693514cd3f9e2f3eea60fc8b693333
GS-Bacon <55279200+GS-Bacon@users.noreply.github.com>
2022-12-05 22:31:24 +0900

    RobotMining

--- MiningRobot.lua
+++ MiningRobot.lua
@@ -1,11 +1,27 @@
 local robot = require("robot")
 robot.forward()
 
-local toolslot=1
+local ToolSlot=1
+local StartMaker=2
 
 local function MiningFront()
-        if robot.detect()==true then
-            robot.select()
-            robot.swingfront()
-        end
+    if robot.detectFront() then
+        robot.swing()
+        robot.suck()
     end
+end
+
+local function MiningUp()
+    if robot.detectUp() then
+        robot.swingUp()
+        robot.suckUp()
+    end
+end
+
+while true do
+    MiningFront()
+    if robot.detectFront()then
+        robot.forward()
+    end
+    MiningUp()
+end
